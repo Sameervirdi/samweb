4a65e12b5e30e01cbf833896227cb3ae15b52f0e
Sameer Virdi <samvirdi@outlook.com>
2018-09-14 19:03:26 -0400

    Sameer's patch to adventure

--- game/adventure.py
+++ game/adventure.py
@@ -7,12 +7,14 @@ Basics:
 2.  Have a list of options and paths to move through game
 3.  Read user input to move down a path
 4.  Add monsters and fight logic
-
 """
 
 
 import prompt_toolkit
 from prompt_toolkit import prompt
+import random
+from random import randint
+
 
 class Player:
     _name = "no_name"
@@ -44,6 +46,12 @@ class Player:
     def is_dead(self):
         return self._health_points <= 0
 
+    def player_attack(slef, GodZilla):
+        GodZilla.lower_hp_monster(self._damage)
+        print("{} has damaged the monster".format(self.name))
+        if GodZilla.monster_dead():
+            print("{} has killed the monster".format(self.name))
+
 
 class GodZilla:
     """
@@ -53,25 +61,29 @@ class GodZilla:
         * maximum damage they can inflict
 
     A single method:
-        * an attack method, which accepts the player and deducts
+        * an attack method, which acceptsthe player and deducts
           maximum damage from the player's hitpoints
     """
+
     def __init__(self, name, health=100):
         self._name = name
         self._health_points = health
-        self._damage = 20
-
+        # self._damage = 20
+        self._damage = randint(10, 50)
 
     def attack(self, player):
         player.lower_hp(self._damage)
         print("you have lost {} of health".format(self._damage))
         if player.is_dead():
-            print('you are dead')
+            print("you are dead")
         else:
-            print('you are damaged')
+            print("you are damaged")
 
+    def lower_hp_monster(self, points=1):
+        self._health_points -= points
 
-
+    def monster_dead(self):
+        return self._health_points <= 0
 
 
 class Caves:
@@ -80,19 +92,27 @@ class Caves:
     'story' is free text that is printed to the user for what they can do or see
     'exits' is just the list of cave state's that are available to move to
     """
-    cave_state = {0: {'name': "at the start of cave", 
-                      'story': "You are at the start of the cave.  You see two paths ahead.",
-                      'monsters': [],
-                      'exits': [1,2] },
-                  1: {'name': "in a dark tunnel",
-                      'story': "You are in a tunnel with two exits.",
-                      'monsters': [GodZilla('bob')],
-                      'exits': [0, 2]},
-                  2: {'name': 'at the end of the cave',
-                      'story': "You have exited the cave.",
-                      'monsters': [],
-                      'exits': None}}
 
+    cave_state = {
+        0: {
+            "name": "at the start of cave",
+            "story": "You are at the start of the cave.  You see two paths ahead.",
+            "monsters": [],
+            "exits": [1, 2],
+        },
+        1: {
+            "name": "in a dark tunnel",
+            "story": "You are in a tunnel with two exits.",
+            "monsters": [GodZilla("bob")],
+            "exits": [0, 2],
+        },
+        2: {
+            "name": "at the end of the cave",
+            "story": "You have exited the cave.",
+            "monsters": [],
+            "exits": None,
+        },
+    }
 
     def __init__(self):
         self._state = 0
@@ -101,13 +121,13 @@ class Caves:
         cur_state = self.cave_state[self._state]
         print("\n")
         print("=" * 40)
-        print("{} is {}".format(player.name(), cur_state['name']))
+        print("{} is {}".format(player.name(), cur_state["name"]))
         print("Where do you want to go?")
-        exit_points = cur_state['exits']
+        exit_points = cur_state["exits"]
 
         for exit_option in exit_points:
             option_details = self.cave_state[exit_option]
-            print("{}: Go {}".format(exit_option, option_details['name']))
+            print("{}: Go {}".format(exit_option, option_details["name"]))
 
         option = prompt("Pick an option: ")
 
@@ -129,17 +149,18 @@ class Caves:
 
     def monster_attack(self, player):
         cur_state = self.cave_state[self._state]
-        for monster in cur_state['monsters']:
+        for monster in cur_state["monsters"]:
             monster.attack(player)
+
 
 def get_username():
     # Prompt the user for their name
-    text = prompt('What is your name? ')
+    text = prompt("What is your name? ")
     return text
 
 
 # This is where the code 'starts'
-if __name__ == '__main__':
+if __name__ == "__main__":
     user_name = get_username()
     p = Player(user_name, 30)
     caves = Caves()
@@ -150,7 +171,3 @@ if __name__ == '__main__':
 
     print("Your final stats are below:")
     print(str(p))
-
-
-
-
